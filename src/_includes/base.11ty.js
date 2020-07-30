122d40bcbcab133e7702280f2d8e3e4253b19cca
Luke Storry <luke.storry@ghyston.com>
2020-07-30 03:14:26 +0100

    linted

--- src/_includes/base.11ty.js
+++ src/_includes/base.11ty.js
@@ -1,20 +1,33 @@
-exports.render = function (data) {
-  return `
-  <!doctype html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="A mapping of the Bristol Womens' Voice Womanifesto to the Bristol Sustainable Development Goals"/>
-        <title>${data.title ? data.title + " | " : ""}BWV-SDGs</title>
-        <link rel="shortcut icon" href="/favicon.ico" type="image/icon"/>
-        <link rel="stylesheet" href="/style.css"/>
-      </head>
-      <body class="bg-gray-800 w-full h-full min-h-screen">
-        <main class="max-w-screen-xl bg-white w-full h-full mx-auto">
-          ${data.content}
-        <main>
-      </body>
-    </html>
-    `;
-};
+exports.render = (data) => `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+      name="description"
+      content="A mapping of the Bristol Womens' Voice Womanifesto to the Bristol Sustainable Development Goals"
+    />
+    <title>${data.title ? data.title + " | " : ""}BWV-SDGs</title>
+    <link rel="shortcut icon" href="/favicon.ico" type="image/icon" />
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+
+  <body class="
+          bg-gray-800
+          w-full
+          h-full
+          min-h-screen
+          ">
+    <main class="
+            max-w-screen-xl
+            bg-white
+            w-full
+            h-full
+            mx-auto
+            ">
+      ${data.content}
+    </main>
+  </body>
+</html>
+`;
--- src/home.11ty.js
+++ src/home.11ty.js
@@ -4,22 +4,29 @@ exports.data = {
   permalink: "/",
 };
 
-const sectionBox = ({ heading, color }) =>
-  `<a href="/${heading.toLowerCase()}/"
+const sectionBox = ({ heading, color }) => `
+<a href="/${heading.toLowerCase()}/"
   class="
-    flex justify-center items-center
-    text-2xl 
-    transition-all
-    duration-1000
-    hover:text-5xl
-    focus:text-5xl
-    ${color}
-  ">
-    <h2>${heading}</h2>
-  </a>`;
+      flex justify-center items-center
+      text-2xl 
+      transition-all
+      duration-1000
+      hover:text-5xl
+      focus:text-5xl
+      ${color}
+      "
+>
+  <h2>${heading}</h2> </a
+>`;
 
-// <h1 class="font-bold text-lg text-center">${title}</h1>
 exports.render = ({ title, sections }) => `
-  <div class="grid grid-cols-1 sm:grid-cols-2 h-full min-h-screen w-full">
-    ${sections.map(sectionBox).join("")}
-  </div>`;
+<div class="
+      grid
+      grid-cols-1
+      sm:grid-cols-2
+      h-full
+      min-h-screen
+      w-full">
+  ${sections.map(sectionBox).join("")}
+</div>
+`;
