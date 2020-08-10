9fcbec7dd150809d70ecf728e385a115a6e189e0
Luke Storry <luke.storry@ghyston.com>
2020-08-10 13:08:37 +0100

    yet more screen issues for timeline thing

--- src/timeline.11ty.js
+++ src/timeline.11ty.js
@@ -89,8 +89,12 @@ exports.render = ({ timelineItems, pageData }) => `
 }</a>" Womanifesto item and the "${pageData.sdg.title}" SDG goal</h1>
 
   <div
-  class="w-4 mt-8 left-in md:left-1/2 bg-blue-700 sticky top-0 h-screen -mb-screen
-  ${timelineItems.length < 6 ? "md:h-screen-part md:-mb-screen-part" : ""}
+  class="w-4 mt-8 left-in md:left-1/2 bg-blue-700 sticky top-0 
+  ${
+    timelineItems.length < 6
+      ? "h-screen-part -mb-screen-part"
+      : "h-screen -mb-screen"
+  }
   "
   ></div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,12 @@ module.exports = {
     },
     extend: {
       width: { fit: "fit-content" },
-      height: { "screen-part": "70vh" },
+      height: { "screen-part": "80vh" },
       margin: {
         "-28": "-7rem",
         "-full": "-100%",
         "-screen": "-100vh",
-        "-screen-part": "-70vh",
+        "-screen-part": "-80vh",
       },
     },
   },
